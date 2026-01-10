219c4312be3cdd6a88476052fc4d281abe5dcc55
Himanshu Yadav <168503095+Himanshu-2678@users.noreply.github.com>
2026-01-10 22:55:37 +0530

    Added test-insert for DB

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
 from src.data_loader import synthetic_data
 from src.timeline_builder import user_timeline
 from src.decay_detector import detect_decay
 from src.risk_signal import generate_risk_signal
 from src.pipeline import final_result
+from sqlalchemy.orm import Session
+from src.db.session import get_db
+from src.db.models import User, UserEvent
 
 app = FastAPI()
 
@@ -16,6 +19,7 @@ async def root():  # what is async: It allows the function to run asynchronously
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
+
 
 @app.post("/run")
 def run_pipeline(total_users = 70, total_weeks = 12):
@@ -33,3 +37,19 @@ def run_pipeline(total_users = 70, total_weeks = 12):
     }
 
     return results
+
+
+@app.post("/test-insert")
+def test_func(db: Session = Depends(get_db)):
+    user = User(is_active = True)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    event = UserEvent(user_id = user.id, week = 1, activity_score = 0.85)
+
+    db.add(event)
+    db.commit()
+
+    return {"user_id": user.id, "activity_score" : event.activity_score}
+
